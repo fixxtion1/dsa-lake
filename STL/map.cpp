a09b9b1e9510407e0c97be582791ccd8def86bdf
aman043 <dheerajkumarr235@gmail.com>
2022-03-23 18:07:30 +0530

    update

---
+++ STL/map.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+
+void solve(){
+	map<string, int> mp;
+	
+	mp["hello"] = 1;
+	mp["id"] = 1;
+	
+	cout << mp["hello"];
+}
+
+ 
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);	
+	
+	int tc=1;
+	//cin>>tc;
+ 
+	while(tc--) solve();
+	return 0;
+}
